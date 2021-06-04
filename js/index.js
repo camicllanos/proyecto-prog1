97b8f9deedd3dd15b3c41fe3faa322da99257b31
Jeronimo30 <jsojo@udesa.edu.ar>
2021-06-04 12:27:14 -0300

    empezando js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let buscador = document.querySelector(`.buscador1`);
+let mensaje = " ";
+
+if (buscador){
+    mensaje = "El campo esta vacio"
+} else if (buscador){
+    mensaje = "Al menos 3 caracteres"
+} else if(buscador);
