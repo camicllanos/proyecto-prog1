fa1be071dbc75951b16213a71cc4096e467faf56
camicllanos <cllanos@udesa.edu.ar>
2021-06-14 00:08:25 -0300

    agrego demas fetch js

--- detalle-js/album.js
+++ detalle-js/album.js
@@ -1,2 +1,20 @@
 alert ("hola");
-let url= "https://api.deezer.com/album/302127"
+let urlAlbum= "https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127"
+fetch(urlArtist)
+    .then(function(response){
+        return response.json();
+    })
+    .then( function(data){
+        //Aca va mi codigo//
+        
+        //capturamos los destinos//
+        let  = document.querySelector(".")
+        let  = document.querySelector (".");
+        let  = document.querySelector (".");
+        let  = document.querySelector (".");
+
+        
+    })
+    .catch(function (error){
+        console.log (error);
+    })
---
+++ detalle-js/artist.js
@@ -0,0 +1,19 @@
+let urlArtist = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/27"
+fetch(urlArtist)
+    .then(function(response){
+        return response.json();
+    })
+    .then( function(data){
+        //Aca va mi codigo//
+        
+        //capturamos los destinos//
+        let  = document.querySelector(".")
+        let  = document.querySelector (".");
+        let  = document.querySelector (".");
+        let  = document.querySelector (".");
+
+        
+    })
+    .catch(function (error){
+        console.log (error);
+    })
