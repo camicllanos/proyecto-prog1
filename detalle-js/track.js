e4074b99bd9ddccd4fb2e84355a7ac1bfe39d497
camicllanos <cllanos@udesa.edu.ar>
2021-06-15 17:27:29 -0300

    actualizo js

--- detalle-js/track.js
+++ detalle-js/track.js
@@ -5,14 +5,18 @@ fetch(urlTracks)
     })
     .then( function(data){
         //Aca va mi codigo//
-        
+        let tracks = data.album[0];
         //capturamos los destinos//
-        let avatar = document.querySelector(".avatar")
+
+        let trackAvatar = document.querySelector(".avatar")
         let nombreCancion = document.querySelector (".nombrecancion");
         let nombreArtista = document.querySelector (".nombreartsita");
         let nombreDisco = document.querySelector (".nombredisco");
         console.log (data);
-        
+
+        trackAvatar.src =  tracks.album.cover_medium; 
+        nombreCancion.innerHTML += tracks.title;
+             
     })
     .catch(function (error){
         console.log (error);
