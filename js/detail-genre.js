ebf142b4f8e062126d769ea18c0c64ce27d2cae1
Jeronimo30 <jsojo@udesa.edu.ar>
2021-06-18 11:27:05 -0300

    termine parte genero y su detalle

--- js/detail-genre.js
+++ js/detail-genre.js
@@ -1,4 +1,39 @@
 let query = location.search;
 let queryObject = new URLSearchParams(query);
-let id = queryObject.get("id")
+let id = queryObject.get("id");
 
+let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}`;
+
+fetch (url)
+    .then (function (response){
+        return response.json();
+    })
+    .then (function (data){
+        //console.log(data);
+        let genre = document.querySelector (".tipo-genero")
+
+        genre.innerText = data.name;
+
+    })
+    .catch( e => { console.log(e) })
+
+
+let url1 = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}/artists`;
+
+fetch (url1)
+    .then (function (response){
+        return response.json();
+    })
+    .then (function (data){
+        console.log(data);
+        let info = data.data
+        let detailGenre = document.querySelector (".lista-det1")
+
+        for (let i=0; i<4; i++){
+            detailGenre.innerHTML += `<li class="genero">
+                                        <img src= "${info[i].picture}">
+                                        <p>"${info[i].name}"</p>
+                                    </li>`
+        }
+    })
+    .catch( e => { console.log(e) })
--- js/genre.js
+++ js/genre.js
@@ -1,4 +1,4 @@
-let url = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre"
+let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre`;
 
 fetch (url)
     .then (function (response){
@@ -6,17 +6,15 @@ fetch (url)
     })
     .then (function (data){
         console.log(data);
-
-        console.log(data.data)
-        console.log(data.data[1])
         let arrayInfo = data.data
         let lista = document.querySelector (".listas")
 
         for (let i=1; i<5; i++){
-            lista.innerHTML += `<li>
+            lista.innerHTML += `<li class="genero">
+                                    <img src="${arrayInfo[i].picture}">
+                                    <p> ${arrayInfo[i].name}</p>
                                     <a href="detail-genres.html?id=${arrayInfo[i].id}">
-                                    ${arrayInfo[i].name} <img src="${arrayInfo[i].picture}">
-                                    </a>
+                                    Ver más</a>
                                 </li>`
         }
     })
