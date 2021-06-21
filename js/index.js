1f984d6bad8e59a3c95ea208ef5a014a4d8d6612
AgustinaKicillof <akicillofsabaris@udesa.edu.ar>
2021-06-21 14:14:26 -0300

    Avance Fin de semama Index + Resultado de Busqueda

--- js/index.js
+++ js/index.js
@@ -1,79 +1,149 @@
 
+
 // Header y Footer
 
+// manejo del input de búsqueda
 let formBusqueda = document.getElementById("formBusqueda")
 let inputBusqueda = document.getElementById("inputBusqueda")
-
 
 formBusqueda.addEventListener('submit', function (event) {
 
     event.preventDefault()
     let busqueda = inputBusqueda.value.trim()
-    
- // Aca estamos armando las condiciones para el buscador 
-
+    //                 operador OR
     if (busqueda.length < 3 || busqueda === '') {
-        alert("Ingreseo inválido")
-
+        alert("Ingreso inválido")
     } else {
 
-        let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${busqueda}`
+        // guardar la búsqueda en localStorage
+        localStorage.setItem('searchAPI', busqueda)
 
-
-        fetch(url)
-            .then( function (response) { 
-                return response.json() 
-            } )
-            .then( function (respuesta) {
-    
-                // acá escribo todo lo que quiero hacer con la respuesta
-                console.log(respuesta.data[0])
-
-
-
-            })
-
+        // redirecciona a la página de search
+        window.location.replace('./search-results.html')
     }
-  
+
 })
 
+// Fin Header y Footer
 
-let buscador = document.querySelector(`.buscador1`);
-let mensaje = " ";
+// Home del Sitio 
 
-if (buscador != null){
-    mensaje = "El campo esta vacio"
-} else if (buscador.length < 3){
-    mensaje = "Al menos 3 caracteres"
-} else if(buscador);
+let favoritos = []
 
-//TRACKS//
-let urlTracks = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks`
+let favEnLS = JSON.parse(localStorage.getItem('favoritos'))
+// verificar que haya favoritos cargados y mantenerlos
+if (favEnLS) {
+    favoritos = favEnLS
+}
 
-fetch (urlTracks)
-    .then(function(response){
-        return response.json();
-    })
-    .then( function(data){
-        //Aca va mi codigo//
-        let arrayTracks = data.data;
-        let topTracks = document.querySelector(".listaBestTrack");
-        let info = "";
-        console.log(data);
 
-        for(let i=0; i<5; i++)
-            info += ` <article>
-            <a href="detail-track.html?id=${arrayTracks[i].id}">
-            <img class= "trackAvatar" src="${arrayTracks[i].album.cover_medium}" alt="Cancion">
-            <h3><a class="nombreArtista" href="detail-track.html?id=${arrayTracks[i].artist.id}">${arrayTracks[i].artist.name}</a></h3>
-            <h4><a href="detail-track.html?id=${arrayTracks[i].id}">${arrayTracks[i].title}</a></h4>
-            </article>`
+llamarIndexAPI()
 
-            topTracks.innerHTML += info; 
-        
-    })
-    .catch(function (error){
-        console.log (error);
-})
-//ALBUM
+function llamarIndexAPI() {
+    // activar el loader
+    let loader = document.getElementById('loader-container')
+    loader.style.display = "flex"
+    let proxy = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/`
+
+    fetch(proxy)
+        .then(function (response) { return response.json() })
+        .then(function (respuesta) {
+            // acá escribo todo lo que quiero hacer con la respuesta
+            console.log(respuesta)
+
+            // desestructuro la respuesta en distintos arreglos
+            let albumsApi = respuesta.albums.data
+            let artistasApi = respuesta.artists.data
+            let tracksApi = respuesta.tracks.data
+
+            // resultados en DOM
+            // selectores de las UL
+            let listaAlbums = document.querySelector('#lista-albums')
+            let listaArtistas = document.querySelector('#lista-artistas')
+            let listaTracks = document.querySelector('#lista-tracks')
+
+            // for para crear la lista de Albums
+            listaAlbums.innerHTML = ''
+            for (let i = 0; i < albumsApi.length; i++) {
+                listaAlbums.innerHTML += `
+                    <li class="caja album">
+                        <a href="detail-album.html"><i><img src=${albumsApi[i].cover_big} alt=${albumsApi[i].title}></i></a>
+                        <p>${albumsApi[i].title}</p>
+                    </li>
+                    `
+            }
+
+            // for para crear la lista de Artistas
+            listaArtistas.innerHTML = ''
+            for (let i = 0; i < artistasApi.length; i++) {
+                listaArtistas.innerHTML += `
+                    <li class="caja artista">
+                        <a href="detail-album.html"><i><img src=${artistasApi[i].cover_big} alt=${artistasApi[i].title}></i></a>
+                        <p>${artistasApi[i].title}</p>
+                    </li>
+                    `
+            }
+
+            // for para crear la lista de Canciones
+            listaTracks.innerHTML = ''
+            for (let i = 0; i < tracksApi.length; i++) {
+                listaTracks.innerHTML += `
+                    <li class="caja cancion">
+                        <a href="detail-album.html"><i><img src=${tracksApi[i].cover_big} alt=${tracksApi[i].title}></i></a>
+                        <p>${tracksApi[i].title}</p>
+                        <button class="btn-favoritos">Agregar a favoritos</button>
+                    </li>
+                    `
+            }
+
+
+            // seleccionamos todos los LI y les asignamos evento para redireccionarse a la pág correspondiente
+            // manda el título del elemento al localStorage para cargarlo en otra página.
+            let albums = document.querySelectorAll('.album')
+
+            for (let i = 0; i < albums.length; i++) {
+                albums[i].addEventListener('click', function () {
+                    let titulo = albums[i].children[1].innerText
+                    localStorage.setItem('albumAPI', titulo)
+                })
+            }
+
+            let artistas = document.querySelectorAll('.artista')
+
+            for (let i = 0; i < artistas.length; i++) {
+                artistas[i].addEventListener('click', function () {
+                    let artista = artistas[i].children[1].innerText
+                    localStorage.setItem('artistaAPI', artista)
+                })
+            }
+
+            let canciones = document.querySelectorAll('.cancion')
+
+            for (let i = 0; i < canciones.length; i++) {
+                canciones[i].addEventListener('click', function () {
+                    let cancion = canciones[i].children[1].innerText
+                    localStorage.setItem('cancionAPI', cancion)
+                })
+            }
+
+            // asignar evento a botones de favoritos
+
+            let botonesFavoritos = document.querySelectorAll('.btn-favoritos')
+            // rescata img y titulo y guarda en local storage
+            for (let i = 0; i < botonesFavoritos.length; i++) {
+                botonesFavoritos[i].addEventListener('click', function (event) {
+                    let img = event.target.parentElement.children[0].src
+                    let titulo = event.target.parentElement.children[1].innerText
+
+                    favoritos.push({ imagen: img, title: titulo })
+
+                    localStorage.setItem('favoritos', JSON.stringify(favoritos))
+                })
+            }
+            // desactivar el loader
+            loader.style.display = "none"
+        })
+
+}
+
 
---
+++ js/search-results.js
@@ -0,0 +1,115 @@
+
+// Header y Footer
+
+// manejo del input de búsqueda
+let formBusqueda = document.getElementById("formBusqueda")
+let inputBusqueda = document.getElementById("inputBusqueda")
+
+formBusqueda.addEventListener('submit', function (event) {
+
+    event.preventDefault()
+    let busqueda = inputBusqueda.value.trim()
+    //                 operador OR
+    if (busqueda.length < 3 || busqueda === '') {
+        alert("Ingreso inválido")
+    } else {
+
+        // guardar la búsqueda en localStorage
+        localStorage.setItem('searchAPI', busqueda)
+
+        // redirecciona a la página de search
+        window.location.replace('./search-results.html')
+    }
+
+})
+
+// Fin Header y Footer
+
+// revisar el localStorage para obtener búsqueda de otra página
+
+let busquedaEnLS = localStorage.getItem('searchAPI')
+// si hay algo en LS, hago el llamado a la api para mostrar resultados
+if (busquedaEnLS) {
+    llamarAPI(busquedaEnLS)
+    localStorage.removeItem('searchAPI')
+}
+
+
+
+// manejo del input de búsqueda
+let formBusqueda = document.getElementById("formBusqueda")
+let inputBusqueda = document.getElementById("inputBusqueda")
+
+formBusqueda.addEventListener('submit', function (event) {
+
+    event.preventDefault()
+    let busqueda = inputBusqueda.value.trim()
+    //                 operador OR
+    if (busqueda.length < 3 || busqueda === '') {
+        alert("Ingreso inválido")
+    } else {
+        llamarAPI(busqueda)
+    }
+  
+})
+
+
+
+// funcion para llamar a la API y mostrar resultados
+function llamarAPI(busqueda) {
+    // activar el loader
+    let loader = document.getElementById('loader-container')
+    loader.style.display = "flex"
+
+
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${busqueda}`)
+            .then( function (response) { return response.json() } )
+            .then( function (respuesta) {   
+                // acá escribo todo lo que quiero hacer con la respuesta
+                console.log(respuesta.data[0])
+                // resultados en DOM
+                
+                let contenedorBusquedas = document.getElementById('resultadosBusqueda')        
+                let tituloBusquedas = document.getElementById('tituloResultado')
+
+                if (respuesta.data.length > 0) {
+
+                    tituloBusquedas.innerHTML = `Resultados de búsqueda para ${busqueda}`
+
+                    // vacío el contenedor antes de agregar nuevas búsquedas
+                    // para que no se acumulen
+                    contenedorBusquedas.innerHTML = ''
+                    
+                    // crear la lista de canciones con los resultados de la api
+                    for (let i = 0; i < 10; i++) {                       
+                        contenedorBusquedas.innerHTML += `
+                            <li class="caja cancion">
+                                <a href="detail-track.html"><i><img src=${respuesta.data[i].album.cover_big} alt=${respuesta.data[i].title}></i></a>
+                                <p>${respuesta.data[i].title}</p>
+                            </li>
+                        `
+                    }
+
+
+                    let canciones = document.querySelectorAll('.cancion')
+
+                    for (let i = 0; i < canciones.length; i++) {
+                        canciones[i].addEventListener('click', function() {
+                            
+                            let cancion = canciones[i].children[1].innerText
+                            localStorage.setItem('cancionAPI', cancion)
+                        })
+                    }
+
+
+                } else {
+                    contenedorBusquedas.innerHTML = ''
+                    tituloBusquedas.innerHTML = `No hay resultados para ${busqueda}`
+                }
+
+
+                // desactivar el loader
+                loader.style.display = "none"
+            })
+            
+}
